0d4d039cd454923caf73c1a38eb41e5443eaac68
igfrv <ilyagafarov@bk.ru>
2024-03-03 16:56:41 +0600

    initial commit

---
+++ rsbuild.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    title: 'Book Library',
+    favicon: './src/assets/icons/icon.svg',
+  },
+})
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.scss'
+
+export default function App() {
+  return (
+    <div className='content'>
+      <h1>Countries Project</h1>
+    </div>
+  )
+}
